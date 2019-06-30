c6d3af223e5f558581dc48d066c31f65e7e56755
Naoto Furuhata_lab <s1411473@u.tsukuba.ac.jp>
2019-06-30 17:00:46 +0900

    ２つのカメラを利用

---
+++ src/two_image_converter.cpp
@@ -0,0 +1,66 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+static const std::string OPENCV_WINDOW = "Image window";
+
+class ImageConverter{
+	// メンバ変数
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	image_transport::Publisher image_pub_;
+
+public:
+	ImageConverter() // コンストラクタ
+		: it_(nh_) // メンバ変数の初期化
+	{
+		// subscrive to input video feed and publish ouput video feed
+		image_sub_ = it_.subscribe("/cv_camera/image_raw", 1, &ImageConverter::imageCb, this);
+		image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+		cv::namedWindow(OPENCV_WINDOW);
+	}
+
+	~ImageConverter() // デストラクタ
+	{
+		cv::destroyWindow(OPENCV_WINDOW);
+	}
+
+	void imageCb(const sensor_msgs::ImageConstPtr& msg) // メンバ関数
+	{
+		cv_bridge::CvImagePtr cv_ptr;
+		// try catch 例外処理
+		try
+		{
+			cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+		}
+		catch (cv_bridge::Exception& e)
+		{
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
+
+		// Draw an example circle on the video stream
+		if(cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+			cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255, 0, 0));
+
+		// Update GUI Window
+		cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+		cv::waitKey(3);
+
+		// Output modified video stream
+		image_pub_.publish(cv_ptr->toImageMsg());
+	}
+};
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "image_converter");
+	ImageConverter ic0;
+	ImageConverter ic1;
+	ros::spin();
+	return 0;
+}
